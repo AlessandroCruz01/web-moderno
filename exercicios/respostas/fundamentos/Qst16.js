ba64eefc89cc01d864111d58bd332000db6fefaf
Alessandro Cruz <noobcuriosoac@gmail.com>
2020-11-17 21:56:40 -0500

    Ate o 17

--- exercicios/respostas/fundamentos/Qst16.js
+++ exercicios/respostas/fundamentos/Qst16.js
@@ -1,2 +1,27 @@
-/*16)​ Utilizando a estrutura do Switch faça um programa que simule uma calculadora básicaO programa recebecomo parâmetros dois valores numéricos e uma string referente à operação e a realize com os valores
-numéricos na ordem que foram inseridos. Por exemplo: calculadora (2, ‘+’, 3). A função efetuará a soma de 2 e3. Dica: Os sinais das operações são: ‘+’. ‘-’, ‘*’ e ‘/’.  Crie um caso default para operações inválidas. */
+/*16)​ Utilizando a estrutura do Switch faça um programa que simule uma calculadora básica. O programa recebe como parâmetros dois valores numéricos e uma string referente à operação e a realize com os valores
+numéricos na ordem que foram inseridos. Por exemplo: calculadora (2, ‘+’, 3). A função efetuará a soma de 2 e 3. Dica: Os sinais das operações são: ‘+’. ‘-’, ‘*’ e ‘/’.  Crie um caso default para operações inválidas. */
+
+const calculadora = (n1 = 0, n2 = 0, op) => {
+    switch(op) {
+        case '+':
+            return `Soma: ${n1} + ${n2} = ${n1+n2}`
+            break
+        case '-':
+            return `Subtração: ${n1} - ${n2} = ${n1-n2}`
+            break
+        case '*':
+            return `Multiplicação: ${n1} x ${n2} = ${n1*n2}`
+            break
+        case '/':
+            return `Divisão: ${n1} / ${n2} = ${n1/n2}`
+            break
+        default:
+            return 'Inválido!'
+    }
+}
+
+console.log(calculadora(5, 7, '*'))
+console.log(calculadora(5, 7, '+'))
+console.log(calculadora(5, 7, '/'))
+console.log(calculadora(5, 7, '-'))
+console.log(calculadora(5, 7, '**'))
---
+++ exercicios/respostas/fundamentos/Qst17.js
@@ -0,0 +1,5 @@
+/*17)​ Um funcionário irá receber um aumento de acordo com o seu plano detrabalho, de acordo com a tabela abaixo: Plano Aumento A10% B15% C20% Faça uma função que leia o plano de trabalho e o salário atual de um funcionário e calcula e imprime o seu novo salário. Use a estrutura switch e faça um caso default que indique que o plano é inválido*/
+
+const aumento = () => {
+    
+}
