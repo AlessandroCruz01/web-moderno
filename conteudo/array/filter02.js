e52c42337711a2aab160b5b067d6dbd0bd11028b
Alessandro Cruz <noobcuriosoac@gmail.com>
2021-01-02 19:35:31 -0500

    filter aula 1 e 2

--- conteudo/array/filter02.js
+++ conteudo/array/filter02.js
@@ -1,32 +1,32 @@
 Array.prototype.filter2 = function(callback) {
     const retorno = new Array
-    for (let i in this){
-        // if (callback(this[i], i, this) === true){
-        //     retorno.push(this[i])
-        // }
-        // return retorno
-        return i
+    for (let i = 0 ; i < this.length ; i++){
+        if ((callback(this[i], i , this)) === true){
+            retorno.push(this[i])
+        }
     }
+    
+    return retorno
 }
-
 
 const produtos = [
     { nome: 'Notebook', preco: 2499, fragil: true },
-    { nome: 'Ipad', preco: 4199, fragil: true },
+    { nome: 'Ipad', preco: 4199, fragil: false },
     { nome: 'Copo de vidro', preco: 12.49, fragil: true },
     { nome: 'Copo de plastico', preco: 18.49, fragil: true }
 ]
 
-
 const caro = produto => produto.preco >= 500
 const fragil = produto => produto.fragil
 
+const filtro = function(e){
+    if ((e.fragil === true) && (e.preco >= 500)){
+        return true
+    }
+}
+
 console.log(produtos.filter2(caro))
+console.log(produtos.filter2(fragil))
+console.log(produtos.filter2(filtro))
+console.log(produtos.filter2(caro).filter2(fragil))
 
-// const filtro = function(e){
-//     if ((e.fragil === true) && (e.preco >= 500)){
-//         return true
-//     }
-// }
-
-// console.log(produtos.filter2(filtro))
---
+++ conteudo/array/reduce01.js
@@ -0,0 +1,7 @@
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+const alunos = [
+    {nome: 'Joao', nota: 7.6, bolsista: false},
+    {nome: 'Maria', nota: 9.2, bolsista: true},
+    {nome: 'Pedro', nota: 7.8, bolsista: false},
+    {nome: 'Ana', nota: 8.6, bolsista: false}
+]
