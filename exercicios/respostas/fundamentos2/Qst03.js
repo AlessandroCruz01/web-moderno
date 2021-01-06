7a71be2b7ce15c944ac8eb7de1eeedece254ddde
Alessandro Cruz <62727627+AlessandroCruz01@users.noreply.github.com>
2021-01-05 22:16:20 -0500

    Ate a questao 6

---
+++ exercicios/respostas/fundamentos2/Qst03.js
@@ -0,0 +1,5 @@
+const calculaSalario = (horas, valor) => {
+    return `salario igual á ${horas * valor} R$`
+}
+
+console.log(calculaSalario(150, 40.5))
---
+++ exercicios/respostas/fundamentos2/Qst05.js
@@ -0,0 +1,11 @@
+const maiorOuIgual = (a, b) => {
+    if (a === b){
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(maiorOuIgual(0, 0))
+console.log(maiorOuIgual(0, "0"))
+console.log(maiorOuIgual(5, 1))
