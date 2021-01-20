1eb733a5402bfa857da9902c18ee95026fb15a48
Alessandro Cruz <alessandrocruzadm01@gmail.com>
2021-01-19 22:36:09 -0500

    Finalizando mini projeto usando express

--- conteudo/node/projeto/src/banco.js
+++ conteudo/node/projeto/src/banco.js
@@ -3,3 +3,28 @@ const sequence = {
     get id() { return this._id++ }
 }
 
+const produtos = {}
+
+function salvarProduto(produto) {
+    if (!produto.id) produto.id = sequence.id //Se o id do produto nao estiver cetado, ele vai chamar o sequence entregando um id
+    produtos[produto.id] = produto //perceba existe produtos e produto, produtos é referente ao objeto criado acima e produto é referente ao novo produto a ser cetado!
+    return produto
+}
+
+function getProduto(id){
+    return produtos[id] || {}
+}
+
+function getTodosProdutos() {
+    return Object.values(produtos)
+}
+
+function excluirProduto(id){
+    const produto = produtos[id]
+    delete produtos[id]
+    return produto
+}
+
+module.exports = { salvarProduto, getProduto, getTodosProdutos, excluirProduto }//perceba que os 3 metodos estao cetados para fora, porem o metodo sequence é algo interno deste modulo!
+
+//voltemos para o arquivo server.js
--- conteudo/node/projeto/src/server.js
+++ conteudo/node/projeto/src/server.js
@@ -1,12 +1,51 @@
 const porta = 3003
 
+const bancoDeDados = require('./banco')
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
+app.use( bodyParser.urlencoded({ extended: true }) )//Isso quer dizer que toda requisicao, que tiver um padrao urlencoded ele vai aplicar esse middleware, que vai fazer o parser no corpo da requisicao. ou seja vai tornar o corpo da requisicao web em objetos
+
 app.get('/produtos', (req, res, next) => {
-    res.send({ nome: 'Notebook', preco: 123.45 }) //O metodo send ja converte automaticamente para JSON
+    res.send( bancoDeDados.getTodosProdutos() ) //O metodo send ja converte automaticamente para JSON
 })
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send( bancoDeDados.getProduto(req.params.id))//no caso como vamos ver um id, ele vem na requisicao!
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+
+    res.send(produto)//JSON
+
+})
+
+app.put('/produtos/:id', (req, res, next) => {//para alteração
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+
+    res.send(produto)//JSON
+
+})
+
+app.delete('/produtos/:id', (req, res, next) => {//para exclusao
+    const produto = bancoDeDados.excluirProduto( req.params.id )
+    res.send(produto)//JSON
+})
+
 
 app.listen(porta, () => {
     console.log(`Servidor está executando na porta ${porta}`)
 })
+
+//para nossos testes vamos instalar o body-parser, essa biblioteca serve para conseguir ler o body de uma requisicao!
+
+//vamos para o postman
