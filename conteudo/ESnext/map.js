6564a61a8cb6d7855f099b2d64ace89b9506f616
Alessandro Cruz <62727627+AlessandroCruz01@users.noreply.github.com>
2021-01-26 23:44:30 -0500

    Finalizando map

--- conteudo/ESnext/map.js
+++ conteudo/ESnext/map.js
@@ -15,3 +15,9 @@ chavesVariadas.forEach((vl, ch) => {
     console.log(ch, vl)
 })
 
+console.log(chavesVariadas.has(123))
+chavesVariadas.delete(123)
+console.log(chavesVariadas.has(123))
+console.log(chavesVariadas.size)
+
+chavesVariadas.set(123, 'a')
