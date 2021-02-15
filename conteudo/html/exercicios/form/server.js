7be3ae6277e39f0e40c26dd830a4748d5dbb1f05
Alessandro Cruz <62727627+AlessandroCruz01@users.noreply.github.com>
2021-02-15 16:00:43 -0500

    Finalizando form

--- conteudo/html/exercicios/form/server.js
+++ conteudo/html/exercicios/form/server.js
@@ -6,7 +6,13 @@ app.use(bodyParser.urlencoded( { extended: true } ))
 
 app.post('/usuarios', (req, resp) => {
     console.log(req.body)
-    resp.send('<h1> Parabns </h1>')
+    resp.send('<h1> Deu certo! </h1>')
+})
+
+app.post('/usuarios/:id', (req, resp) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1> Deu certo! Usuarioi alterado </h1>')
 })
 
 app.listen(3003)
